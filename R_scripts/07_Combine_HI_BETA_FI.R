dce06d1cb768c523f51da40897ef5edf1d3d0598
Jake Cavaiani <jcavaiani@alaska.edu>
2022-09-12 15:07:53 -0800

    script update

--- R_scripts/07_Combine_HI_BETA_FI.R
+++ R_scripts/07_Combine_HI_BETA_FI.R
@@ -519,6 +519,144 @@ HI_FI <- left_join(HI_FI, antecedent_2020, by = c("site.ID", "storm.ID", "respon
 write.csv(HI_FI, "~/Documents/Storms_clean_repo/Output_from_analysis/07_Combine_HI_BETA_FI/antecedent_HI_FI_2020.csv")
 
 
+######################### 2021 ####
+# HI ####
+# calculate 95% bootstrap around median of Hyst. Indicies for each site and storm #
+
+median_cl_boot <- function(x, conf = 0.95) {
+  lconf <- (1 - conf)/2
+  uconf <- 1 - lconf
+  require(boot)
+  bmedian <- function(x, ind) median(x[ind])
+  bt <- boot(x, bmedian, 10000)
+  bb <- boot.ci(bt, conf = 0.95, type = "perc")
+  data.frame(y = median(x), ymin = quantile(bt$t, lconf), ymax = quantile(bt$t, 
+                                                                          uconf))
+}
+
+# FRCH #
+FRCH.HI.df <- read.csv("~/Documents/Storms_clean_repo/Output_from_analysis/03_HI_FI/2021/FRCH/FRCH.HI.df.csv")
+
+storm.list = unique(FRCH.HI.df$storm.ID)
+FRCH.HI.boot <- do.call(rbind.data.frame,
+                        lapply(storm.list, function(i){
+                          dat = subset(FRCH.HI.df, storm.ID == i)
+                          median_cl_boot(dat$HI)
+                        }))
+FRCH.HI.boot$storm.ID = storm.list
+
+# MOOS #
+MOOS.HI.df <- read.csv("~/Documents/Storms_clean_repo/Output_from_analysis/03_HI_FI/2021/MOOS/MOOS.HI.df.csv")
+
+storm.list = unique(MOOS.HI.df$storm.ID)
+MOOS.HI.boot <- do.call(rbind.data.frame,
+                        lapply(storm.list, function(i){
+                          dat = subset(MOOS.HI.df, storm.ID == i)
+                          median_cl_boot(dat$HI)
+                        }))
+MOOS.HI.boot$storm.ID = storm.list
+
+# CARI #
+CARI.HI.df <- read.csv("~/Documents/Storms_clean_repo/Output_from_analysis/03_HI_FI/2021/CARI/CARI.HI.df.csv")
+
+storm.list = unique(CARI.HI.df$storm.ID)
+CARI.HI.boot <- do.call(rbind.data.frame,
+                        lapply(storm.list, function(i){
+                          dat = subset(CARI.HI.df, storm.ID == i)
+                          median_cl_boot(dat$HI)
+                        }))
+CARI.HI.boot$storm.ID = storm.list
+
+# POKE #
+POKE.HI.df <- read.csv("~/Documents/Storms_clean_repo/Output_from_analysis/03_HI_FI/2021/POKE/POKE.HI.df.csv")
+
+storm.list = unique(POKE.HI.df$storm.ID)
+POKE.HI.boot <- do.call(rbind.data.frame,
+                        lapply(storm.list, function(i){
+                          dat = subset(POKE.HI.df, storm.ID == i)
+                          median_cl_boot(dat$HI)
+                        }))
+POKE.HI.boot$storm.ID = storm.list
+
+# VAUL #
+VAUL.HI.df <- read.csv("~/Documents/Storms_clean_repo/Output_from_analysis/03_HI_FI/2021/VAUL/VAUL.HI.df.csv")
+
+storm.list = unique(VAUL.HI.df$storm.ID)
+VAUL.HI.boot <- do.call(rbind.data.frame,
+                        lapply(storm.list, function(i){
+                          dat = subset(VAUL.HI.df, storm.ID == i)
+                          median_cl_boot(dat$HI)
+                        }))
+VAUL.HI.boot$storm.ID = storm.list
+
+# STRT #
+STRT.HI.df <- read.csv("~/Documents/Storms_clean_repo/Output_from_analysis/03_HI_FI/2021/STRT/STRT.HI.df.csv")
+
+storm.list = unique(STRT.HI.df$storm.ID)
+STRT.HI.boot <- do.call(rbind.data.frame,
+                        lapply(storm.list, function(i){
+                          dat = subset(STRT.HI.df, storm.ID == i)
+                          median_cl_boot(dat$HI)
+                        }))
+STRT.HI.boot$storm.ID = storm.list
+
+
+# join data #
+
+FRCH.HI.boot$site.ID = "FRCH"
+MOOS.HI.boot$site.ID = "MOOS"
+CARI.HI.boot$site.ID = "CARI"
+POKE.HI.boot$site.ID = "POKE"
+VAUL.HI.boot$site.ID = "VAUL"
+STRT.HI.boot$site.ID = "STRT"
+
+HI = rbind(FRCH.HI.boot, MOOS.HI.boot, CARI.HI.boot,
+           POKE.HI.boot, VAUL.HI.boot, STRT.HI.boot)
+
+all.FI.diff.results = read.csv("~/Documents/Storms_clean_repo/Output_from_analysis/05_FI/all.FI.diff.results_2021.csv", header = T, row.names = 1)
+
+FI = subset(all.FI.diff.results, select=c("Flushing_index", "percCI_2.5", "percCI_97.5", "ID"))
+FI$ID = as.character(FI$ID)
+FI = separate(FI, ID, into=c("site.ID", "storm.ID", "month", "day", "response_var", NA), sep = "_")
+names(FI) = c("Flush_index", "FI_ymin", "FI_ymax","site.ID", "storm.ID", "month", "day", "response_var")
+
+HI$site.ID=NULL
+HI = separate(HI, storm.ID, into=c("site.ID", "storm.ID", "month", "day", "response_var"), sep = "_")
+names(HI) = c("Hyst_index", "HI_ymin", "HI_ymax","site.ID", "storm.ID", "month", "day", "response_var")
+
+HI_FI = left_join(HI, FI, by=c("site.ID", "storm.ID", "response_var"))
+HI_FI$year <- "2021"
+#write.csv(HI_FI, "~/Documents/Storms_clean_repo/Output_from_analysis/07_Combine_HI_BETA_FI/HI_FI.diff_results.2021.csv")
+
+### BETA ####
+beta_2021 <- read_csv("~/Documents/Storms_clean_repo/Output_from_analysis/06_BETA/beta.2021.csv")
+beta_2021$year <- "2021"
+
+beta_2021 <- beta_2021 %>% 
+  filter(Parameter != "(Intercept)")
+
+names(beta_2021) = c("X1", "site.ID", "storm.ID","Parameter",
+                     "Beta_index", "SE", "CI", "Beta_ymin",
+                     "Beta_ymax", "t", "df", "p", "response_var",
+                     "year")
+
+
+### ANTECEDENT ####
+antecedent_2021 <- read_csv("~/Documents/Storms_clean_repo/Output_from_analysis/04_Antecedent_Conditions/2021/HI.2021.csv")
+antecedent_2021 <- antecedent_2021[,-c(1)]
+
+antecedent_2021$date <- as.Date(antecedent_2021$doy, origin = "2021-01-01")
+origin_date <- as.Date("2021-05-08")
+antecedent_2021$TimeSinceChena <- julian(antecedent_2021$date, origin_date)
+
+names(antecedent_2021)[names(antecedent_2021) == "storm.num"] <- "storm.ID"
+names(antecedent_2021)[names(antecedent_2021) == "response"] <- "response_var"
+antecedent_2021$year <- as.character(antecedent_2021$year)
+# merge ####
+HI_FI = left_join(HI_FI, beta_2021, by=c("site.ID", "storm.ID", "response_var", "year"))
+HI_FI <- left_join(HI_FI, antecedent_2021, by = c("site.ID", "storm.ID", "response_var", "year"))
+
+write.csv(HI_FI, "~/Documents/Storms_clean_repo/Output_from_analysis/07_Combine_HI_BETA_FI/antecedent_HI_FI_2021.csv")
 
 
 #### ALL YEARS #######
@@ -534,10 +672,6 @@ write.csv(HI_FI, "~/Documents/Storms_clean_repo/Output_from_analysis/07_Combine_
 
 # 2018 csv is all good! It handle all the summaries with ease! go 2018!
 
-#2019
-AMC_2019 <- read.csv("~/Documents/Storms_clean_repo/Output_from_analysis/07_Combine_HI_BETA_FI/antecedent_HI_FI_2019.csv")
-
-
 # # Duration 
 # # STRT storm 2
 # AMC_2019$TOTAL.TIME <- ifelse(AMC_2019$site.ID == "STRT" & AMC_2019$storm.ID == "storm2",
@@ -547,15 +681,16 @@ AMC_2019 <- read.csv("~/Documents/Storms_clean_repo/Output_from_analysis/07_Comb
 #                               71.25, AMC_2019$TOTAL.TIME)
 #                       
 
-#2020
-AMC_2020 <- read.csv("~/Documents/Storms_clean_repo/Output_from_analysis/07_Combine_HI_BETA_FI/antecedent_HI_FI_2020.csv")
 
+AMC_2018 <- read_csv("Output_from_analysis/07_Combine_HI_BETA_FI/antecedent_HI_FI_2018.csv")
+AMC_2019 <- read_csv("Output_from_analysis/07_Combine_HI_BETA_FI/antecedent_HI_FI_2019.csv")
+AMC_2020 <- read_csv("Output_from_analysis/07_Combine_HI_BETA_FI/antecedent_HI_FI_2020.csv")
+AMC_2021 <- read_csv("Output_from_analysis/07_Combine_HI_BETA_FI/antecedent_HI_FI_2021.csv", 
+                                  col_types = cols(date = col_date(format = "%m/%d/%Y")))
 
+AMC <- rbind(AMC_2018, AMC_2019, AMC_2020, AMC_2021)
 
-
-
-
-
+write.csv(AMC, "~/Documents/Storms_clean_repo/Output_from_analysis/07_Combine_HI_BETA_FI/antecedent_HI_FI_AllYears.csv")
 
 
 
